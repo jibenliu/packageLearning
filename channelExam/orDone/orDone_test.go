c58392a9a22829c577daba0aa37b351a1d10fa8a
jibenliu <jibenliu@126.com>
2021-09-06 16:17:41 +0800

    doc: 调整doc

--- channelExam/orDone/orDone_test.go
+++ channelExam/orDone/orDone_test.go
@@ -16,16 +16,16 @@ func BenchmarkOr(b *testing.B) {
 	}
 }
 
-func BenchmarkOrInReflect(b *testing.B) {
+func BenchmarkInReflect(b *testing.B) {
 	done := make(chan interface{})
 	defer close(done)
 	num := 100
 	streams := make([]<-chan interface{}, num)
 	for i := range streams {
-		streams[i] = OrInReflect(done)
+		streams[i] = repeat(done, []int{1, 2, 3})
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		<-Or(streams...)
+		<-OrInReflect(streams...)
 	}
 }
