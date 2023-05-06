7a137c725c8e335795e1bd0e2bcf25946676ae28
刘志兵 <jibenliu@126.com>
2023-05-06 14:44:41 +0800

    修复报错

--- socks5Exam/auth_socks5/client/client.go
+++ socks5Exam/auth_socks5/client/client.go
@@ -1,26 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/gamexg/proxyclient"
-	"io"
 )
 
 func main() {
-	p, err := proxyclient.NewProxyClient("socks5://root:password@127.0.0.1:8888")
+	p, err := proxyclient.NewProxyClient("socks5://root:password@111.67.195.179:9898")
 	if err != nil {
 		panic(err)
 	}
 
-	c, err := p.Dial("tcp", "www.accebits.com:443")
+	c, err := p.Dial("tcp", "www.accebits.com:80")
 	if err != nil {
 		panic(err)
 	}
 
-	io.WriteString(c, "GET /index.html HTTP/1.0\r\nHOST:www.163.com\r\n\r\n")
-	b, err := io.ReadAll(c)
-	if err != nil {
+	// 发送 HTTP 请求
+	req := "GET / HTTP/1.1\r\n" +
+		"Host: accebits.com\r\n" +
+		"Connection: close\r\n\r\n"
+	fmt.Fprintf(c, req)
+
+	// 读取响应
+	scanner := bufio.NewScanner(c)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
-	fmt.Print(string(b))
 }
