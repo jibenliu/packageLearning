eb53833b5783f5a977dd1946406b7423bede9cac
刘志兵 <jibenliu@126.com>
2023-07-13 16:17:18 +0800

    整理代码

--- fileExam/writeByteFile.go
+++ fileExam/writeByteFile.go
@@ -4,25 +4,25 @@ import (
 	"bytes"
 	"encoding/gob"
 	"fmt"
-	"io/ioutil"
+	"os"
 )
 
 type Article struct {
-	Id int
-	Title string
+	Id      int
+	Title   string
 	Content string
-	Author string
+	Author  string
 }
 
 // 写入二进制数据到磁盘文件
-func write(data interface{}, filename string)  {
+func write(data interface{}, filename string) {
 	buffer := new(bytes.Buffer)
 	encoder := gob.NewEncoder(buffer)
 	err := encoder.Encode(data)
 	if err != nil {
 		panic(err)
 	}
-	err = ioutil.WriteFile(filename, buffer.Bytes(), 0600)
+	err = os.WriteFile(filename, buffer.Bytes(), 0600)
 	if err != nil {
 		panic(err)
 	}
@@ -30,7 +30,7 @@ func write(data interface{}, filename string)  {
 
 // 从磁盘文件加载二进制数据
 func read(data interface{}, filename string) {
-	raw, err := ioutil.ReadFile(filename)
+	raw, err := os.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
@@ -42,12 +42,12 @@ func read(data interface{}, filename string) {
 	}
 }
 
-func main()  {
+func main() {
 	article := Article{
-		Id: 1,
-		Title: "基于 Gob 包编解码二进制数据",
+		Id:      1,
+		Title:   "基于 Gob 包编解码二进制数据",
 		Content: "通过 Gob 包序列化二进制数据以便通过网络传输",
-		Author: "学院君",
+		Author:  "学院君",
 	}
 	write(article, "article_data")
 	var articleData Article
